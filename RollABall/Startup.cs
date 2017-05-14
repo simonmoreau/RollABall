e87dfcce38038b2a4d483f44743f68fec91bcedc
Simon Moreau <simon@bim42.com>
2017-05-14 21:09:59 +0200

    First Commit
    First Commit

---
+++ RollABall/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace RollABall
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            StaticFileOptions option = new StaticFileOptions();
+            FileExtensionContentTypeProvider contentTypeProvider = (FileExtensionContentTypeProvider)option.ContentTypeProvider ??
+            new FileExtensionContentTypeProvider();
+
+            contentTypeProvider.Mappings.Add(".mem", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".data", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".memgz", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".datagz", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".unity3dgz", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".unityweb", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".jsgz", "application/x-javascript; charset=UTF-8");
+
+            option.ContentTypeProvider = contentTypeProvider;
+
+            app.UseStaticFiles(option);
+
+            //app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
